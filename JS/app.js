7b062352b24f50b0492d8b5780166293b790cc90
Hellemond Dargalaii <hdargalaii@gmail.com>
2021-03-28 20:10:39 -0700

    added first for loop

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,9 @@
 let todolist = [{ todo: "clone repo for starter code", status:"complete" }, { todo: "push repo to github", status:"started" }]
 
 let completedtodo = []
+
+for (let item of todolist){
+    if (item.status === "complete")
+    completedtodo.push(item)
+
+}
